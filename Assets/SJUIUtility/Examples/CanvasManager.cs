c6d3b121c50bd2f96e6ec7abc3063d0171922f5d
Sungjin <saswillneverdie@gmail.com>
2017-03-13 02:47:25 -0400

    SJUIUtility

---
+++ Assets/SJUIUtility/Examples/CanvasManager.cs
@@ -0,0 +1,118 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using System.Collections;
+
+using YourSungjin.UI;
+
+public class CanvasManager : MonoBehaviour
+{
+	[SerializeField] private CanvasGroup groupA, groupB, groupC;
+
+	void Awake ()
+	{
+		//bind buttons
+		Button[] groupAButtons = groupA.GetComponentsInChildren<Button> (true);
+		foreach (Button button in groupAButtons) {  
+			Button temp = button;
+			button.onClick.AddListener (() => OnClickGroupAButton (temp));
+		}
+
+		Button[] groupBButtons = groupB.GetComponentsInChildren<Button> (true);
+		foreach (Button button in groupBButtons) {  
+			Button temp = button;
+			button.onClick.AddListener (() => OnClickGroupBButton (temp));
+		}
+
+		Button[] groupCButtons = groupC.GetComponentsInChildren<Button> (true);
+		foreach (Button button in groupCButtons) {  
+			Button temp = button;
+			button.onClick.AddListener (() => OnClickGroupCButton (temp));
+		}
+	}
+
+	void Start ()
+	{ 
+		InitUI ();
+	}
+
+
+	private void InitUI ()
+	{
+		groupA.gameObject.SetActive (true);
+		groupB.gameObject.SetActive (false);
+		groupC.gameObject.SetActive (false);
+	}
+
+
+	void OnDestroy ()
+	{
+		//unbind buttons
+		Button[] groupAButtons = groupA.GetComponentsInChildren<Button> (true);
+		foreach (Button button in groupAButtons) {  
+			Button temp = button;
+			button.onClick.RemoveListener (() => OnClickGroupAButton (temp));
+		}
+
+		Button[] groupBButtons = groupB.GetComponentsInChildren<Button> (true);
+		foreach (Button button in groupBButtons) {  
+			Button temp = button;
+			button.onClick.RemoveListener (() => OnClickGroupBButton (temp));
+		}
+
+		Button[] groupCButtons = groupC.GetComponentsInChildren<Button> (true);
+		foreach (Button button in groupCButtons) {  
+			Button temp = button;
+			button.onClick.RemoveListener (() => OnClickGroupCButton (temp));
+		}
+	}
+
+
+	#region EventHandler
+
+	private void OnClickGroupAButton (Button sender)
+	{  
+		switch (sender.transform.name) {  
+		case "AtoBButton":  
+			SJUIUtility.ShowUI (this, groupA, false, 0, .5f, delegate() {
+				SJUIUtility.ShowUI (this, groupB, true, 0, .5f);
+			});
+			break;
+
+		case "AtoBtoCButton":  
+			SJUIUtility.ShowUI (this, groupA, false, 0, .5f, delegate() {
+				SJUIUtility.ShowUI (this, groupB, true, 0, .5f, delegate() {
+					SJUIUtility.ShowUI (this, groupC, true, 0, .5f); 
+				});
+			});
+			break;
+
+		default:  
+			break;  
+		}  
+	}
+
+	private void OnClickGroupBButton (Button sender)
+	{  
+		switch (sender.transform.name) {  
+		case "DoneButton":  
+			InitUI();
+			break;
+		default:  
+			break;  
+		}  
+	}
+
+	private void OnClickGroupCButton (Button sender)
+	{  
+		switch (sender.transform.name) {  
+		case "DoneButton":  
+			InitUI();
+			break;
+		default:  
+			break;  
+		} 
+	}
+		
+	#endregion
+}
---
+++ Assets/SJUIUtility/Scripts/SJUIUtility.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace YourSungjin.UI
+{
+
+	public static class SJUIUtility
+	{
+
+		public delegate void ShowUIDelegate ();
+
+		static public void ShowUI (MonoBehaviour monoBehaviour, CanvasGroup cg, bool show, float delayTime, float durationTime, ShowUIDelegate showUIComplete = null)
+		{
+			monoBehaviour.StartCoroutine (UITransition (cg, show, delayTime, durationTime, showUIComplete));
+		}
+
+		static private IEnumerator UITransition (CanvasGroup cg, bool show, float delayTime, float durationTime, ShowUIDelegate showUIComplete)
+		{
+
+			yield return new WaitForSeconds (delayTime);
+
+			float elapsedTime = 0;
+			float start, end;
+			cg.gameObject.SetActive (true);
+
+			if (show) {
+				start = 0f;
+				end = 1f;
+			} else {
+				start = 1f;
+				end = 0f;
+			}
+
+			while (elapsedTime < durationTime) {
+				cg.alpha = Mathf.Lerp (start, end, elapsedTime / durationTime);
+				elapsedTime += Time.deltaTime;
+				yield return null;
+			}
+
+			//cg.alpha = end;
+			cg.alpha = 1f; //whether the UI gets activated or not, cg.alpha should always be 1f for later use.
+			cg.gameObject.SetActive (show);
+
+			if (showUIComplete != null)
+				showUIComplete ();
+		}
+
+	}
+}
